96e4c822bd3423f8c96e7a4af54b8013b8e8f694
yeoooooni <qkrtndus3062@naver.com>
2021-07-08 09:30:33 +0900

    Updates

---
+++ precache-manifest.27ce0efba56bc86b397e427a3b26ca58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4942a73a68f4ec523664e26e79a332fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b4a377334a5eb4c5506",
+    "url": "/static/css/main.3bc3c130.chunk.css"
+  },
+  {
+    "revision": "3a17d125cc8914a0e7c2",
+    "url": "/static/js/2.8c260d6a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.8c260d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b4a377334a5eb4c5506",
+    "url": "/static/js/main.86f83c01.chunk.js"
+  },
+  {
+    "revision": "bd7f4f55b55e38cea2f1",
+    "url": "/static/js/runtime-main.58002af7.js"
+  }
+]);
